47fa7ecdf32a3398a0b6efa7da41dfc3214c2f8a
CharlotteGrossmann <charlotte.grossmann@stud.h-da.de>
2019-06-20 18:10:18 +0200

    StepperMotor done for now

---
+++ Regal/StepperMotor/StepperMotor.ino
@@ -0,0 +1,53 @@
+// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
+
+#include <Stepper.h>
+
+#define STEPS 2038 // the number of steps in one revolution of your motor (28BYJ-48)
+
+Stepper stepper(STEPS, 8, 10, 9, 11);
+const int triggerpin = 12;     
+const int echopin = 13;     
+long time;                    //Variable for storing the time traveled
+int S;                        //Variable for storing the distance covered   
+int show;
+void setup() {
+  pinMode(triggerpin, OUTPUT);  //Setting the triggerpin as output pin
+  pinMode(echopin, INPUT);      //Setting the echo pin as input pin
+  Serial.begin(9600);
+}
+
+void loop() {
+  if(S <5){
+    if(show==0){
+      for(int i=0; i<4; i++){
+        Serial.print("i: ");
+        Serial.println(i);
+        stepper.setSpeed(18); // 1 rpm
+        stepper.step(500); // do 2038 steps -- corresponds to one revolution in one minute
+      }
+    }
+    show=1;
+  }
+
+  else{
+   if(show==1){
+     for(int j=0; j<4; j++){
+        Serial.print("j: ");
+        Serial.println(j);
+        stepper.setSpeed(18); // 1 rpm
+        stepper.step(-500); // do 2038 steps -- corresponds to one revolution in one minute
+      }
+    }
+    show=0;
+  }
+  digitalWrite(triggerpin, LOW);  
+  delayMicroseconds(2);
+  digitalWrite(triggerpin, HIGH); //Setting the triggerpin high for 10us to generate a wave
+  delayMicroseconds(10);
+  digitalWrite(triggerpin, LOW);
+  time = pulseIn(echopin, HIGH); //Setting the echopin high to receive the wave
+  S= time*0.034/2;                //Calculating the distance traveled in cm
+  Serial.print("Distance: ");
+  Serial.println(S);
+  
+}
