10c515a86307b1eeadd658f440c8209374967b91
MatzeDietrich <matthias.dietrich@stud.h-da.de>
2019-06-21 21:16:48 +0200

    added basic navigation/orientation

--- iDoku2/js/main.js
+++ iDoku2/js/main.js
@@ -1,6 +1,9 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
   const sectionContainer = document.getElementById('sectionContainer')
   const sections = document.getElementsByTagName('section')
+  const navAnchors = document.getElementsByClassName('navAnchor')
   const sectionsArray = Array.prototype.slice.call(sections)
   const wagen_s = document.getElementById('wagen_section')
   const warenkorb = document.getElementById('warenkorb')
@@ -10,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function scrollAnimation () {
     console.log('scrolling')
     const scrollPos = sectionContainer.scrollLeft
-    const maxScroll = sectionContainer.offsetWidth*1.5
+    const maxScroll = sectionContainer.offsetWidth * 1.5
     const currentSectionIndex = Math.floor((scrollPos / maxScroll))
     console.log(scrollPos / maxScroll)
     const currentSection = sections[Math.floor(scrollPos / maxScroll)]
@@ -19,58 +22,43 @@ document.addEventListener('DOMContentLoaded', function () {
       }
       else
         warenkorb.classList.add('fixedWarenkorb')
-    }
-    else{
-        warenkorb.classList.remove('fixedWarenkorb')
+    }else {
+      warenkorb.classList.remove('fixedWarenkorb')
     }
 
-    for (var i = 0; i <= sectionsArray.length; i++) {
+    for (var i = 0; i <= sections.length; i++) {
       if (currentSectionIndex == i) {
         sections.item(i).classList.add('active')
+        navAnchors.item(i).classList.add('active')
       }else {
         if (sections.item(i).classList.contains('active'))
           sections.item(i).classList.remove('active')
+        navAnchors.item(i).classList.remove('active')
       }
     }
   }
 
+  if (sectionContainer.addEventListener) {
+    sectionContainer.addEventListener('mousewheel', MouseWheelHandler, false)
+    sectionContainer.addEventListener('DOMMouseScroll', MouseWheelHandler, false)
+  } else sectionContainer.attachEvent('onmousewheel', MouseWheelHandler)
 
-
-
-
-
-
-
-
-
-
-
-
-if (sectionContainer.addEventListener) {
-    sectionContainer.addEventListener("mousewheel", MouseWheelHandler, false);
-    sectionContainer.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
-} else sectionContainer.attachEvent("onmousewheel", MouseWheelHandler);
-
-function MouseWheelHandler(e) {
+  function MouseWheelHandler (e) {
 
     // cross-browser wheel delta
-    var e = window.event || e;
-    var delta = - 20 * (Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))));
+    var e = window.event || e
+    var delta = - 20 * (Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail))))
 
-    var pst = sectionContainer.scrollLeft += delta;
+    var pst = sectionContainer.scrollLeft += delta
 
     if (pst < 0) {
-        pst = 0;
+      pst = 0
     } else if (pst > sectionContainer.width()) {
-        pst = sectionContainer.width();
+      pst = sectionContainer.width()
     }
 
-    sectionContainer.scrollLeft = pst;
+    sectionContainer.scrollLeft = pst
 
-    return false;
-}
-
-
-
-  
+    return false
+  }
 })
