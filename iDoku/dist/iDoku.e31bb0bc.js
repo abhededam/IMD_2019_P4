bec5e21df6312f0e07f29e3b9eae2360e4fa4d92
Aileen Bo Hededam <aileen.b.hededam@stud.h-da.de>
2019-06-23 20:59:29 +0200

    i dont f know

--- iDoku/dist/iDoku.e31bb0bc.js
+++ iDoku/dist/iDoku.e31bb0bc.js
@@ -154,7 +154,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 });
-},{}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"C:/Users/ailee/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -182,7 +182,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64362" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61662" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -357,5 +357,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
+},{}]},{},["C:/Users/ailee/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
 //# sourceMappingURL=/iDoku.e31bb0bc.js.map
